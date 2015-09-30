faa6ce07869ae676f1fdacd856304b5cf2f764cd
itzikberrebi <itzikb800@gmail.com>
2015-09-30 19:41:26 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function() {
 	jQuery('.expand').on('click', function(e)  {
 
 		$.ajax({
-			url: "http://checkurl.phishtank.com/checkurl/index.php?format=xml&app_key=c30f2407e6a0e74e234ff502558335686786570ae2de338547e396ef736c7c78&url=http://www.torec.co.il",
+			url: "http://checkurl.phishtank.com/checkurl/index.php?format=json&app_key=c30f2407e6a0e74e234ff502558335686786570ae2de338547e396ef736c7c78&url=http://www.torec.co.il",
 			// data: myData,
 			type: 'GET',
 			crossDomain: true, // enable this

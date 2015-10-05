99cffdc1be79862ff4d6a15d6de640222f39f355
itzikberrebi <itzikb800@gmail.com>
2015-10-05 10:22:20 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -14,29 +14,50 @@ jQuery(document).ready(function() {
 
 	jQuery('.expand').on('click', function(e)  {
 
-		$.ajax({
-			xhrFields: {
-				withCredentials: true
-			},
-			url: "http://checkurl.phishtank.com/checkurl/index.php?format=xml&app_key=c30f2407e6a0e74e234ff502558335686786570ae2de338547e396ef736c7c78&url=http://www.torec.co.il&callback=?",
-			// data: myData,
-			type: 'GET',
-			crossDomain: true, // enable this
-			dataType: 'xml',
-			// jsonpCallback: 'phishtank',
-        	// jsonp: false,
-			success: function(result) { alert(result); },
-			error: function (jqXHR, textStatus, errorThrown) {
-				console.log(jqXHR.responseText);
-			}
-			// beforeSend: setHeader
-		});
+	var site = 'http://checkurl.phishtank.com/checkurl/index.php?format=xml&app_key=c30f2407e6a0e74e234ff502558335686786570ae2de338547e396ef736c7c78&url=http://www.torec.co.il';
+    var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=cbFunc';
+
+	$.getJSON( yql, cbFunc );
+     
+    function cbFunc(data) {
+    // If we have something to work with...
+    if ( data.results[0] ) {
+        // Strip out all script tags, for security reasons.
+        // BE VERY CAREFUL. This helps, but we should do more. 
+        data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '');
+         
+        // If the user passed a callback, and it
+        // is a function, call it, and send through the data var.
+        if ( typeof callback === 'function') {
+            callback(data);
+        }
+    }
+    // Else, Maybe we requested a site that doesn't exist, and nothing returned.
+    else throw new Error('Nothing returned from getJSON.');
+    }
+		// $.ajax({
+		// 	xhrFields: {
+		// 		withCredentials: true
+		// 	},
+		// 	url: "&callback=?",
+		// 	// data: myData,
+		// 	type: 'GET',
+		// 	crossDomain: true, // enable this
+		// 	dataType: 'xml',
+		// 	// jsonpCallback: 'phishtank',
+  //       	// jsonp: false,
+		// 	success: function(result) { alert(result); },
+		// 	error: function (jqXHR, textStatus, errorThrown) {
+		// 		console.log(jqXHR.responseText);
+		// 	}
+		// 	// beforeSend: setHeader
+		// });
 	});
 
-	function phishtank (data) {
+	// function phishtank (data) {
 
-    alert(data);
-    console.log(data);
-	};
+ //    alert(data);
+ //    console.log(data);
+	// };
 
 });
